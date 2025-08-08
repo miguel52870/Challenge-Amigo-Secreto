7123988c91823d6c6f171aa9805435b4ec1a9b5b
Miguel52870 <miguel52870@gmail.com>
2025-08-08 12:35:21 -0600

    se crearon las funciones para limpiar caja y añadir nombres

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ ingresar nombres de sus amigos y luego realizar un sorteo aleatorio para determi
 en una lista visible en la página, y al finalizar, un botón "Sortear Amigo" seleccionará uno de los nombres de forma aleatoria, 
 mostrando el resultado en pantalla.
 
--Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto y lo agregarán a una lista visible al hacer clic en "Adicionar".
+
--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. 
+// Aquí deberás desarrollar la lógica para resolver el problema.
+
+//-Agregar nombres: Los usuarios escribirán el nombre de un amigo en un campo de texto 
+// y lo agregarán a una lista visible al hacer clic en "Adicionar".
+//se crea la lista para almacenar los nombres de los amigos añadidos
+let Amigos = [];
+
+
+function limpiarCaja(){
+    document.getElementById('amigo').value='';
+}
+
+function agregarAmigo(){
+    let nombre = document.getElementById('amigo').value.trim();
+
+    if(nombre === ''){
+        alert('ingrese un nombre valido');
+        return;
+    }
+    else{
+        Amigos.push(nombre);
+        limpiarCaja();
+    }
+}
+
+
+
