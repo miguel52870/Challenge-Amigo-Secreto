08030ac7de0b77cbde62e55c9776a840a508b3f3
Miguel52870 <miguel52870@gmail.com>
2025-08-08 13:20:26 -0600

    se agregó la funcion para actualizar y mostrar la lista

--- app.js
+++ app.js
@@ -6,21 +6,37 @@
 //se crea la lista para almacenar los nombres de los amigos añadidos
 let Amigos = [];
 
-
+//esta funcion limpia el cuadro para agregar los nombres
 function limpiarCaja(){
     document.getElementById('amigo').value='';
 }
 
+//esta funcion agrega elementos a la lista "amigos"
 function agregarAmigo(){
     let nombre = document.getElementById('amigo').value.trim();
 
+
     if(nombre === ''){
-        alert('ingrese un nombre valido');
+        alert('por favor inserte un nombre');
         return;
     }
     else{
         Amigos.push(nombre);
         limpiarCaja();
+        mostrarLista();//llama a la funcion para mostrar lista en la interfaz
+    }    
+}
+
+//se crea una función para actualizar la lista de amigos
+
+function mostrarLista(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    for(let i = 0; i <Amigos.length; i++){
+        let li = document.createElement('li');
+        li.textContent = Amigos[i];
+        lista.appendChild(li);
     }
 }
 
